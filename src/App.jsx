c04c5f975481095fab36ecebee6211913a463a01
Dikshant Thapa <dikshantthapa3@gmail.com>
2023-12-23 10:55:04 +0545

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dikshant Bikram Thapa",
+  title: "Machine Learning Engineer",
+  email: "dikshantthapa3@gmail.com",
+  gitHub: "dikshantthapaa3",
+  instagram: "dikshant_thapa_",
+  linkedIn: "DikshantBikramThapa",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
